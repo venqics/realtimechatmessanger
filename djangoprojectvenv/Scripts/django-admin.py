c711c0a6506f21983f6fb935581b330ad225f619
venqics <venx.rao87@gmail.com>
2021-04-06 17:57:20 +0530

    #1.1 intial project setup

---
+++ djangoprojectvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\venky\Documents\real_time_chat_messanger\djangoprojectvenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
