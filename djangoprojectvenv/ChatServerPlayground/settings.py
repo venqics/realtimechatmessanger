94a4a7f8311f33be4cff8f27f420af538a9e94b8
venqics <venx.rao87@gmail.com>
2021-04-19 14:10:57 +0530

    #1.5 added static and media files settings

--- djangoprojectvenv/ChatServerPlayground/settings.py
+++ djangoprojectvenv/ChatServerPlayground/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     #My apps
     'core',
-    
+
     #Third party apps
     'django.contrib.admin',
     'django.contrib.auth',
@@ -140,3 +140,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static/'),
+        os.path.join(BASE_DIR,'media/'),
+]
+
+
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
+
+TEMP = os.path.join(BASE_DIR, 'media_cdn/temp')
+
+BASE_URL = "http://127.0.0.1:8000"
