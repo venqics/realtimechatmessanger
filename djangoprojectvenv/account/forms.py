6ebc708f0c601fc72937c23a178ad5db711ff36f
venqics <venx.rao87@gmail.com>
2021-04-20 11:51:02 +0530

    #1.8 enabling user for login functionality

--- djangoprojectvenv/account/forms.py
+++ djangoprojectvenv/account/forms.py
@@ -29,5 +29,18 @@ class RegistrationForm(UserCreationForm):
 		raise forms.ValidationError('Username "%s" is already in use.' % username)
 
 
+class AccountAuthenticationForm(forms.ModelForm):
 
+	password = forms.CharField(label='Password', widget=forms.PasswordInput)
+
+	class Meta:
+		model = Account
+		fields = ('email', 'password')
+
+	def clean(self):
+		if self.is_valid():
+			email = self.cleaned_data['email']
+			password = self.cleaned_data['password']
+			if not authenticate(email=email, password=password):
+				raise forms.ValidationError("Invalid login")
 
--- djangoprojectvenv/account/views.py
+++ djangoprojectvenv/account/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import login, authenticate
 from django.conf import settings
-from .forms import RegistrationForm
+from .forms import RegistrationForm, AccountAuthenticationForm
 # Create your views here.
 
 
@@ -31,3 +31,41 @@ def register_view(request, *args, **kwargs):
 		form = RegistrationForm()
 		context['registration_form'] = form
 	return render(request, 'account/register.html', context)
+
+def login_view(request, *args, **kwargs):
+	context = {}
+
+	user = request.user
+	if user.is_authenticated: 
+		return redirect("home")
+
+	destination = get_redirect_if_exists(request)
+	print("destination: " + str(destination))
+
+	if request.POST:
+		form = AccountAuthenticationForm(request.POST)
+		if form.is_valid():
+			email = request.POST['email']
+			password = request.POST['password']
+			user = authenticate(email=email, password=password)
+
+			if user:
+				login(request, user)
+				if destination:
+					return redirect(destination)
+				return redirect("home")
+
+	else:
+		form = AccountAuthenticationForm()
+
+	context['login_form'] = form
+
+	return render(request, "account/login.html", context)
+
+
+def get_redirect_if_exists(request):
+	redirect = None
+	if request.GET:
+		if request.GET.get("next"):
+			redirect = str(request.GET.get("next"))
+	return redirect
