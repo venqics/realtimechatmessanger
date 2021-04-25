408820064bea0afb869259035d087288275c46a3
venqics <venx.rao87@gmail.com>
2021-04-25 15:15:17 +0530

    #2.7 friend requests view

--- djangoprojectvenv/friend/urls.py
+++ djangoprojectvenv/friend/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from .views import send_friend_request
+from .views import send_friend_request, friend_requests
 
 app_name = 'friend'
 
 urlpatterns = [ 
      path('friend_request/', send_friend_request, name='friend-request'),
+     path('friend_request/<user_id>/', friend_requests, name='friend-requests'),
+
 ]
+
