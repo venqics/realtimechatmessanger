1465377f8a8c52e3c27dec082ccfeb408f1f7073
venqics <venx.rao87@gmail.com>
2021-05-12 23:39:18 +0530

    #3.4 public chat consumer

--- djangoprojectvenv/ChatServerPlayground/routing.py
+++ djangoprojectvenv/ChatServerPlayground/routing.py
@@ -1,12 +1,18 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from django.urls import path
+from django.urls import path, re_path
+from djangoprojectvenv.public_chat.consumers import PublicChatConsumer
+
 
 application = ProtocolTypeRouter({
 	'websocket': AllowedHostsOriginValidator(
 		AuthMiddlewareStack(
 			# URLRouter([...]) # Empty for now because we don't have a consumer yet.
-		)
+		URLRouter([
+					path('public_chat/<room_id>/', PublicChatConsumer),
+			])
+        
+        )
 	),
 })
--- djangoprojectvenv/core/views.py
+++ djangoprojectvenv/core/views.py
@@ -1,3 +1,7 @@
+from django.conf import settings
 from django.shortcuts import render
-
-# Create your views here.
+def home_screen_view(request):
+	context = {}
+	context['debug_mode'] = settings.DEBUG
+	context['room_id'] = "1"
+	return render(request, "core/home.html", context)
