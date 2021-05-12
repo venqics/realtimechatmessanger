ac7a8c02576374c63dc17d1d0f49da0d25e2e4a9
venqics <venx.rao87@gmail.com>
2021-05-12 15:29:17 +0530

    #2.9 decline friend request view

--- djangoprojectvenv/friend/urls.py
+++ djangoprojectvenv/friend/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import send_friend_request, friend_requests,accept_friend_request, remove_friend
+from .views import send_friend_request, friend_requests,accept_friend_request, remove_friend, decline_friend_request
 
 app_name = 'friend'
 
@@ -8,5 +8,6 @@ urlpatterns = [
      path('friend_request/', send_friend_request, name='friend-request'),
      path('friend_request/<user_id>/', friend_requests, name='friend-requests'),
      path('friend_remove/', remove_friend, name='remove-friend'),
+     path('friend_request_decline/<friend_request_id>/', decline_friend_request, name='friend-request-decline'),
 ]
 
