58a1291e30b2207fdc055939183e03b4c07c4e8e
venqics <venx.rao87@gmail.com>
2021-05-12 15:22:56 +0530

    #2.8 remove friend requests view

--- djangoprojectvenv/friend/urls.py
+++ djangoprojectvenv/friend/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from .views import send_friend_request, friend_requests
+from .views import send_friend_request, friend_requests,accept_friend_request, remove_friend
 
 app_name = 'friend'
 
 urlpatterns = [ 
      path('friend_request/', send_friend_request, name='friend-request'),
      path('friend_request/<user_id>/', friend_requests, name='friend-requests'),
-
+     path('friend_remove/', remove_friend, name='remove-friend'),
 ]
 
--- djangoprojectvenv/friend/views.py
+++ djangoprojectvenv/friend/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Account, FriendRequest
+from .models import Account, FriendRequest, FriendList
 from django.http import HttpResponse
 import json
 
@@ -53,4 +53,51 @@ def send_friend_request(request, *args, **kwargs):
 			payload['response'] = "Unable to sent a friend request."
 	else:
 		payload['response'] = "You must be authenticated to send a friend request."
+	return HttpResponse(json.dumps(payload), content_type="application/json")
+
+
+def accept_friend_request(request, *args, **kwargs):
+	user = request.user
+	payload = {}
+	if request.method == "GET" and user.is_authenticated:
+		friend_request_id = kwargs.get("friend_request_id")
+		if friend_request_id:
+			friend_request = FriendRequest.objects.get(pk=friend_request_id)
+			# confirm that is the correct request
+			if friend_request.receiver == user:
+				if friend_request:
+					# found the request. Now accept it
+					updated_notification = friend_request.accept()
+					payload['response'] = "Friend request accepted."
+
+				else:
+					payload['response'] = "Something went wrong."
+			else:
+				payload['response'] = "That is not your request to accept."
+		else:
+			payload['response'] = "Unable to accept that friend request."
+	else:
+		# should never happen
+		payload['response'] = "You must be authenticated to accept a friend request."
+	return HttpResponse(json.dumps(payload), content_type="application/json")
+
+
+def remove_friend(request, *args, **kwargs):
+	user = request.user
+	payload = {}
+	if request.method == "POST" and user.is_authenticated:
+		user_id = request.POST.get("receiver_user_id")
+		if user_id:
+			try:
+				removee = Account.objects.get(pk=user_id)
+				friend_list = FriendList.objects.get(user=user)
+				friend_list.unfriend(removee)
+				payload['response'] = "Successfully removed that friend."
+			except Exception as e:
+				payload['response'] = f"Something went wrong: {str(e)}"
+		else:
+			payload['response'] = "There was an error. Unable to remove that friend."
+	else:
+		# should never happen
+		payload['response'] = "You must be authenticated to remove a friend."
 	return HttpResponse(json.dumps(payload), content_type="application/json")
