6da37d67f558b1cadc63c68c3978a0cd199d35da
venqics <venx.rao87@gmail.com>
2021-05-13 00:23:07 +0530

    #3.5 public chat consumer messages

--- djangoprojectvenv/public_chat/consumers.py
+++ djangoprojectvenv/public_chat/consumers.py
@@ -4,8 +4,26 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+MSG_TYPE_MESSAGE = 0  # For standard messages
+
+
 # Example taken from:
 # https://github.com/andrewgodwin/channels-examples/blob/master/multichat/chat/consumers.py
+
+class ClientError(Exception):
+    """
+    Custom exception class that is caught by the websocket receive()
+    handler and translated into a send back to the client.
+    """
+    def __init__(self, code, message):
+        super().__init__(code)
+        self.code = code
+        if message:
+        	self.message = message
+
+
+
+
 class PublicChatConsumer(AsyncJsonWebsocketConsumer):
 
 	async def connect(self):
@@ -14,7 +32,7 @@ class PublicChatConsumer(AsyncJsonWebsocketConsumer):
 		"""
 		print("PublicChatConsumer: connect: " + str(self.scope["user"]))
 		# let everyone connect. But limit read/write to authenticated users
-		await self.accept()
+		
 
 
 	async def disconnect(self, code):
@@ -34,3 +52,38 @@ class PublicChatConsumer(AsyncJsonWebsocketConsumer):
 		# Messages will have a "command" key we can switch on
 		command = content.get("command", None)
 		print("PublicChatConsumer: receive_json: " + str(command))
+		try:
+			if command == "send":
+				if len(content["message"].lstrip()) != 0:
+					await self.send_room(content["room_id"], content["message"])
+					# raise ClientError(422,"You can't send an empty message.")
+			elif command == "join":
+				# Make them join the room
+				await self.join_room(content["room"])
+			elif command == "leave":
+				# Leave the room
+				await self.leave_room(content["room"])
+			elif command == "get_room_chat_messages":
+				await self.display_progress_bar(True)
+				room = await get_room_or_error(content['room_id'])
+				payload = await get_room_chat_messages(room, content['page_number'])
+				if payload != None:
+					payload = json.loads(payload)
+					await self.send_messages_payload(payload['messages'], payload['new_page_number'])
+				else:
+					raise ClientError(204,"Something went wrong retrieving the chatroom messages.")
+				await self.display_progress_bar(False)
+		except ClientError as e:
+			await self.display_progress_bar(False)
+			await self.handle_client_error(e)
+
+class ClientError(Exception):
+    """
+    Custom exception class that is caught by the websocket receive()
+    handler and translated into a send back to the client.
+    """
+    def __init__(self, code, message):
+        super().__init__(code)
+        self.code = code
+        if message:
+        	self.message = message
