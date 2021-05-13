c94f3a3e0c67f39b640eb5498d6224ef07cade4c
venqics <venx.rao87@gmail.com>
2021-05-14 01:15:18 +0530

    #4.4 Displaying all the friends with messages

--- djangoprojectvenv/chat/views.py
+++ djangoprojectvenv/chat/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from chat.models import PrivateChatRoom, RoomChatMessage
-
+from itertools import chain
 from django.conf import settings
 
 
@@ -12,4 +12,30 @@ def private_chat_room_view(request, *args, **kwargs):
 		return redirect("login")
 
     context = {}
+    
+    # 1. Find all the rooms this user is a part of 
+    rooms1 = PrivateChatRoom.objects.filter(user1=user, is_active=True)
+    rooms2 = PrivateChatRoom.objects.filter(user2=user, is_active=True)
+
+	# 2. merge the lists
+    rooms = list(chain(rooms1, rooms2))
+    print(str(len(rooms)))
+    
+    """
+    m_and_f:
+		[{"message": "hey", "friend": "Mitch"}, {"message": "You there?", "friend": "Blake"},]
+  	Where message = The most recent message
+  """
+    m_and_f = [] 
+    for room in rooms:
+		# Figure out which user is the "other user" (aka friend)
+		  if room.user1 == user:
+			  friend = room.user2
+		  else:
+			  friend = room.user1
+		  m_and_f.append({
+			  'message': "", # blank msg for now (since we have no messages)
+			  'friend': friend
+		  })
+    context['m_and_f'] = m_and_f
     return render(request, "chat/room.html", context)
