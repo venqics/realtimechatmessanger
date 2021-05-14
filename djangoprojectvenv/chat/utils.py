5ce43b5e581d2e876a5426ea26e15eff62fed7c9
venqics <venx.rao87@gmail.com>
2021-05-14 17:10:53 +0530

    #4.7 get user info dynamically

--- djangoprojectvenv/chat/utils.py
+++ djangoprojectvenv/chat/utils.py
@@ -1,4 +1,6 @@
 from chat.models import PrivateChatRoom
+from django.core.serializers.python import Serializer
+
 
 def find_or_create_private_chat(user1, user2):
 	try:
@@ -10,3 +12,16 @@ def find_or_create_private_chat(user1, user2):
 			chat = PrivateChatRoom(user1=user1, user2=user2)
 			chat.save()
 	return chat
+
+
+class LazyRoomChatMessageEncoder(Serializer):
+    def get_dump_object(self, obj):
+        dump_object = {}
+        dump_object.update({'msg_type': MSG_TYPE_MESSAGE})
+        dump_object.update({'msg_id': str(obj.id)})
+        dump_object.update({'user_id': str(obj.user.id)})
+        dump_object.update({'username': str(obj.user.username)})
+        dump_object.update({'message': str(obj.content)})
+        dump_object.update({'profile_image': str(obj.user.profile_image.url)})
+        dump_object.update({'natural_timestamp': calculate_timestamp(obj.timestamp)})
+        return dump_object
